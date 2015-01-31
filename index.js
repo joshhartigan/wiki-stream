6cb90e5c6b143b0a23c48ee4497d5a876ad51081
Josh Hartigan <joshhartigan99@gmail.com>
2015-01-31 12:33:54 +0000

    correct styling

--- index.js
+++ index.js
@@ -17,21 +17,18 @@ if (!search) {
 
 var visitedPages = []
 
-function getWikiPage(name) {
-  var wikiPage = 'http://en.wikipedia.org/wiki/' +
-    name.replace(' ', '_')
+function getWikiPage (name) {
+  var wikiPage = 'http://en.wikipedia.org/wiki/' + name.replace(' ', '_')
   console.log(wikiPage)
 
-  req( wikiPage, function(err, response, data) {
-
+  req(wikiPage, function (err, response, data) {
     if (err) {
       console.log(errorEmoji + err)
       process.exit(1)
     }
 
     if (response.statusCode === 404) {
-      console.log(errorEmoji + ' problem: ' + wikiPage +
-        ' can\'t be found!')
+      console.log(errorEmoji + ' problem: ' + wikiPage + ' can\'t be found!')
       process.exit(1)
     }
 
@@ -39,26 +36,24 @@ function getWikiPage(name) {
       var $ = cheerio.load(data)
 
       var validLinks = $('p>a')
-      var linkIndex = Math.floor( ( Math.random() * $('p>a').length ) + 1 )
+      var linkIndex = Math.floor((Math.random() * $('p>a').length) + 1)
 
       try {
         var firstLinkName = validLinks[linkIndex].children[0].data
         console.log('\nlinks to ' + firstLinkName + '...')
-      } catch(e) {
+      } catch (e) {
         process.exit(1)
       }
 
       var firstLink = validLinks[linkIndex].attribs.href.replace('/wiki/', '')
 
-      if ( visitedPages.indexOf(firstLink) >= 0 ) {
+      if (visitedPages.indexOf(firstLink) >= 0) {
         console.log('You reached full circle! The stream ends.')
       } else {
         visitedPages.push(firstLink)
         getWikiPage(firstLink)
       }
     }
-
   })
 }
-
 
